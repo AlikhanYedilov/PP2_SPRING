ea890663a2d82677c19b6ec0ff5e908ba1b5f81c
Aliko <a_yedilov@kbtu.kz>
2024-02-08 11:17:35 +0600

    2

---
+++ FUNC1/f12.py
@@ -0,0 +1,9 @@
+n = int(input()) # histogram ints
+def aliko(n):
+    for i in range(n):
+        a = int(input())
+        for j in range(a):
+            print("*", end = "")
+        #print("\n")    
+
+aliko(n)
